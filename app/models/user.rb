2598b39f88eeb163d0e6dda6d38955c377899e6c
Apprentice <apprentice@devbootcamp.com>
2013-08-09 15:29:49 -0700

    user signin working

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
+  
+  def self.authenticate(username, password)
+    current_user = User.find_by_username(username)
+    if current_user.nil?
+      return nil
+    elsif current_user.password == password
+      return current_user
+    else
+      return nil
+    end
+  end
+
   has_many :rounds
   has_many :decks, through: :rounds
 end
